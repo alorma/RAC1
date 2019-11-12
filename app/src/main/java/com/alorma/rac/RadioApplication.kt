df7f17a83b0c031166fbf566a3790e622ca579ad
Bernat Borrás <bernat.borras@schibsted.com>
2019-11-12 16:05:36 +0100

    Add dependencies

--- app/src/main/java/com/alorma/rac/RadioApplication.kt
+++ app/src/main/java/com/alorma/rac/RadioApplication.kt
@@ -3,15 +3,19 @@ package com.alorma.rac
 import android.app.Application
 import com.alorma.rac.di.networkModule
 import com.alorma.rac.di.uiModule
+import com.gabrielittner.threetenbp.LazyThreeTen
+import com.sergiandreplace.androiddatetimetextprovider.AndroidDateTimeTextProvider
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.threeten.bp.format.DateTimeTextProvider
 
 class RadioApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-
+        LazyThreeTen.init(this)
+        DateTimeTextProvider.setInitializer(AndroidDateTimeTextProvider())
 
         startKoin {
             androidContext(this@RadioApplication)
